6d2585f8a0904f55f5ef3ae578f16feaaf7bc87a
Banger Thompson <nickthompson23@comcast.net>
2017-11-17 00:45:01 -0700

    Small UI update
    Added basic door state visual update/get

--- js/app.js
+++ js/app.js
@@ -1,6 +1,11 @@
 $(function(){
+	const OCCUPIED = 1;
 	// endpoint
 	var endpoint = "./sys/";
+	var currentBuildings = [];
+	var visibleBuildings = [];
+	var initialized = false;
+	var pollingSpeed = 5000;
 
 	function initializeUI(){
 		//initialize foundation
@@ -17,6 +22,21 @@ $(function(){
 			dots : true,
 			arrows : false
 		});
+		//console.log(currentBuildings);
+
+		if(currentBuildings.length > 0) {
+			initialized = true;
+			// ***** Improve this in the future to only get data for visible buildings, getting all for now
+			visibleBuildings = currentBuildings;
+		}
+
+		setInterval(function() {
+			if(initialized) {
+				visibleBuildings.forEach(function(buildingID) {
+				    getCurrentBuildingRoomStates(buildingID);
+				});			
+			}		    
+		}, pollingSpeed);
 	}//end initializeUi
 
 	// grab area data
@@ -29,9 +49,13 @@ $(function(){
 			,id : 1 
 		}
 	}).done(function(rtn) {
-		console.log(rtn);
+		//console.log(rtn);
 		//parse building list
+		var firstItemClass = 'is-active';
 		rtn.buildings.forEach(function(building){
+			if(currentBuildings.indexOf(building.id) < 0) {
+				currentBuildings.push(building.id);
+			}
 			var slide_string = '<div class="slide" data-building-id="'+building.id+'"><h2>'+building.name+' <small>(Building '+building.number+')</small></h2>';
 				slide_string += '<div class="floorplan-svg">';
 					slide_string += '<?xml version="1.0" encoding="utf-8"?>';
@@ -40,7 +64,7 @@ $(function(){
 				slide_string += '</div>';
 			slide_string += '</div>';
 
-			var accordion_string = '<li class="accordion-item" data-accordion-item>';
+			var accordion_string = '<li class="accordion-item ' + firstItemClass + '" data-accordion-item>';
 				accordion_string += '<a href="#" class="accordion-title" data-building-id="'+building.id+'">'+building.name+' <small>('+building.number+')</small></a>';
 				accordion_string += '<div class="accordion-content" data-tab-content>';
 					accordion_string += '<div class="row small-up-2">';
@@ -59,9 +83,39 @@ $(function(){
 
 			$('[data-building-slider]').append(slide_string);
 			$('[data-accordion]').append(accordion_string);
+			firstItemClass = '';
 		});
 
 		//go go gadget UI
 		initializeUI();
 	});//end ajax request
+
+
+	function getCurrentBuildingRoomStates(buildingid) {
+		// grab room status data
+		$.ajax({
+			method: "GET",
+			url: endpoint,
+			data: {
+				method: 'site'
+				,request: 'getBuildingSensorData'
+				,id : buildingid 
+			}
+		}).done(function(rtn) {
+			//console.log(rtn);
+			rtn.sensors.forEach(function(sensor){
+				var currentDoor = $('[data-door-id="' + sensor.id + '"]');
+				var currentDoorSlider = document.querySelector('#bathroom_' + buildingid + '_' + sensor.id);
+				if(sensor.state == OCCUPIED && !currentDoor.find('.status').hasClass('occupied')) {
+					// Update to active
+					currentDoorSlider.classList.add('occupied');
+					currentDoor.addClass('occupied');
+				} else if(sensor.state != OCCUPIED && currentDoor.find('.status').hasClass('occupied')){
+					// Update to inactive			
+		            currentDoorSlider.classList.removeClass('occupied');
+					currentDoor.removeClass('occupied');
+		        }
+			});
+		});//end ajax request
+	}
 });
